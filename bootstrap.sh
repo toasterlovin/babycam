af015e3caaeabfade766beb9b2361c397696730c
Rico Jones <rico@toasterlovin.com>
2015-08-29 16:05:59 -0700

    Install leiningen when bootstrapping VM

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,3 +8,7 @@ apt-get install -y vim
 
 # Clojure
 apt-get install -y default-jdk
+cd /usr/local/bin
+wget -q https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein
+chmod a+x lein
+sudo -u vagrant lein
