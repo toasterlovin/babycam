5e57cbb5b7c75e9699a74fc731807054b84fa778
Rico Jones <rico@toasterlovin.com>
2016-02-08 23:16:26 -0800

    Install OpenCV system wide

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,37 +2,19 @@
 
 # The Basics
 apt-get update
+apt-get install -y git vim tmux
+
+# OpenCV Dependencies
 apt-get install -y build-essential
-apt-get install -y git
-apt-get install -y vim
+apt-get install -y cmake unzip libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
 
-# Clojure
-apt-get install -y default-jdk
-cd /usr/local/bin
-wget -q https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein
-chmod a+x lein
-sudo -u vagrant lein
-
-# OpenCV: http://docs.opencv.org/doc/tutorials/introduction/clojure_dev_intro/clojure_dev_intro.html
-apt-get install -y unzip
-apt-get install -y cmake
-apt-get install -y ant
-su vagrant <<AS_VAGRANT
-  cd ~
-  mkdir opt && cd opt
-  wget -q https://github.com/Itseez/opencv/archive/3.0.0.zip
-  unzip 3.0.0.zip && rm 3.0.0.zip
-  cd opencv-3.0.0/
-  mkdir build && cd build
-  cmake -DBUILD_SHARED_LIBS=OFF ..
-  make -j8
-  cd ~/opt
-  mkdir clj-opencv/ && cd clj-opencv
-  cp ~/opt/opencv-3.0.0/build/bin/opencv-300.jar .
-  mkdir -p native/linux/x86_64
-  cp ~/opt/opencv-3.0.0/build/lib/libopencv_java300.so native/linux/x86_64
-  jar -cMf opencv-native-300.jar native
-  cd /vagrant
-  lein localrepo install /home/vagrant/opt/clj-opencv/opencv-300.jar opencv/opencv 3.0.0
-  lein localrepo install /home/vagrant/opt/clj-opencv/opencv-native-300.jar opencv/opencv-native 3.0.0
-AS_VAGRANT
+# OpenCV
+cd ~
+wget -q https://github.com/Itseez/opencv/archive/3.1.0.zip
+unzip 3.1.0.zip && rm 3.1.0.zip
+cd opencv-3.1.0
+mkdir build && cd build
+cmake ..
+make
+make install
+cd ~ && rm -rf opencv-3.1.0
