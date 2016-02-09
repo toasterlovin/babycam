e2d30b08c77d40bc397b157d19b51371e39e2c39
Rico Jones <rico@toasterlovin.com>
2016-02-08 20:48:31 -0800

    Remove clojure stuff

--- project.clj
+++
@@ -1,9 +0,0 @@
-(defproject babycam "0.1.0-SNAPSHOT"
-  :description "Spy on thy child"
-  :dependencies [[org.clojure/clojure "1.7.0"]
-		 [opencv/opencv "3.0.0"]
-		 [opencv/opencv-native "3.0.0"]]
-  :plugins [[lein-localrepo "0.5.3"]]
-  :main ^:skip-aot babycam.core
-  :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
